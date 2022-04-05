f4c01522d45d313e126527c3e5b34d46b01a03f8
cruelw1nter <99076325+cruelw1nter@users.noreply.github.com>
2022-04-05 16:37:15 +0300

    adaptive layout header, nav, hero

--- source/js/script.js
+++ source/js/script.js
@@ -1,45 +1,35 @@
-// $(document).ready(function(){
-// 	$('.slider').slick({
-// 		arrows:true,
-// 		dots:false,
-// 		slidesToShow:4,
-// 		autoplay:true,
-// 		speed:1000,
-// 		autoplaySpeed:5000,
-// 		responsive:[
-// 			{
-// 				breakpoint: 1370,
-// 				settings: {
-// 					slidesToShow:3
-// 				}
-// 			},
-//       {
-// 				breakpoint: 1070,
-// 				settings: {
-// 					slidesToShow:2
-// 				}
-// 			},
-// 			{
-// 				breakpoint: 760,
-// 				settings: {
-// 					slidesToShow:1,
-// 		      dots:true,
-//           arrows:false,
-// 				}
-// 			}
-// 		]
-// 	});
-// });
-
-var header = document.getElementById("nav");
-var links = header.getElementsByClassName("navigation__link");
-for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("navigation__link--current");
-    current[0].className = current[0].className.replace(" navigation__link--current", "");
-    this.className += " navigation__link--current";
-  });
-}
+$(document).ready(function(){
+	$('.slider-hero').slick({
+		arrows:false,
+		dots:true,
+		slidesToShow:1,
+		autoplay:true,
+		speed:1000,
+		autoplaySpeed:5000,
+		// responsive:[
+		// 	{
+		// 		breakpoint: 1370,
+		// 		settings: {
+		// 			slidesToShow:3
+		// 		}
+		// 	},
+    //   {
+		// 		breakpoint: 1070,
+		// 		settings: {
+		// 			slidesToShow:2
+		// 		}
+		// 	},
+		// 	{
+		// 		breakpoint: 760,
+		// 		settings: {
+		// 			slidesToShow:1,
+		//       dots:true,
+    //       arrows:false,
+		// 		}
+		// 	}
+		// ]
+	});
+});
 
 var menu = document.querySelector(".navigation__menu");
 var nav = document.querySelector(".navigation__list");
