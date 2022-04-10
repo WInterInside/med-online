dd3b9cb44ff1639918c3fae6a36a4206328c7613
cruelw1nter <99076325+cruelw1nter@users.noreply.github.com>
2022-04-10 18:37:25 +0300

    add footer, services

--- source/js/script.js
+++ source/js/script.js
@@ -102,3 +102,14 @@ servicesBtn.addEventListener("click", function() {
     servicesArrow.classList.add("services__arrow--down");
   }
 });
+
+$(document).ready(function(){
+  $('a[href^="#"]').bind("click", function(e){
+      var anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $(anchor.attr('href')).offset().top - 50
+      }, 1000);
+      e.preventDefault();
+  });
+  return false;
+});
