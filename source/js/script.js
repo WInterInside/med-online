ac56bca5bc6e2a9e2d3871e4acf383a441d0e928
cruelw1nter <99076325+cruelw1nter@users.noreply.github.com>
2022-04-19 17:47:06 +0300

    add article page

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,42 @@
+
+var stickySidebar = $('.sticky');
+var mContent = $('.main-content');
+
+if (stickySidebar.length > 0) {
+  var stickyHeight = stickySidebar.height(),
+      sidebarTop = stickySidebar.offset().top;
+}
+
+// on scroll move the sidebar
+$(window).scroll(function () {
+  if (stickySidebar.length > 0) {
+    var scrollTop = $(window).scrollTop();
+
+    if (sidebarTop < scrollTop) {
+      console.log (scrollTop - sidebarTop);
+      stickySidebar.css('top', scrollTop - sidebarTop);
+
+
+      // stop the sticky sidebar at the footer to avoid overlapping
+      var sidebarBottom = stickySidebar.offset().top + stickyHeight,
+          stickyStop = $('.main-content').offset().top + $('.main-content').height();
+      if (stickyStop < sidebarBottom) {
+        var stopPosition = $('.main-content').height() - (stickyHeight + 145);
+        stickySidebar.css('top', stopPosition);
+      }
+    }
+    else {
+      stickySidebar.css('top', '0');
+    }
+  }
+});
+
+$(window).resize(function () {
+  if (stickySidebar.length > 0) {
+    stickyHeight = stickySidebar.height();
+  }
+});
+
 $(window).scroll(function() {
   $('header').toggleClass('header--shadow', $(this).scrollTop() > 0);
 });
@@ -234,3 +273,4 @@ btn.addEventListener('click', function() {
   btn.innerHTML =
     (btn.innerHTML === 'Все вопросы') ? btn.innerHTML = 'Скрыть' : btn.innerHTML = 'Все вопросы';
 })
+
